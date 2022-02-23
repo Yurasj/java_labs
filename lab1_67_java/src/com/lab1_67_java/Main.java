2a57297199fd1e200d69f3f91288d0434b9eeeb4
Yurasj <yura.rozhankivskyy@gmail.com>
2022-02-23 14:42:05 +0200

    lab1

--- lab1_67_java/src/com/lab1_67_java/Main.java
+++ lab1_67_java/src/com/lab1_67_java/Main.java
@@ -5,7 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Farm Farm1 = new Farm();
         Farm Farm2 = new Farm("Kyiv", 150 );
-        Farm Farm3 = new Farm("Kharkiv", 300 , 100)
+        Farm Farm3 = new Farm("Kharkiv", 300 , 100);
+        System.out.println("Farm1");
+        System.out.println(Farm1);
+        System.out.println("Farm2");
+        System.out.println(Farm2);
+        System.out.println("Farm3");
+        System.out.println(Farm3);
+        System.out.println(Farm.getStaticField());
     }
 
 }
